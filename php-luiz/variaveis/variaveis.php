ebda4490037c2b6a2c02a8d69503554e0a53faab
Luiz H. Menezes <79934587+LuizHenriqueMenezes@users.noreply.github.com>
2023-08-25 16:39:56 -0300

    Update variaveis.php

--- php-luiz/variaveis/variaveis.php
+++ php-luiz/variaveis/variaveis.php
@@ -6,4 +6,15 @@ $variavel02 = 1002;
 $variavel03 = "vai cortionas";
 $pode_ser_qualquer_nome;
 $_1pe = "pra começar com número tem que ter _"
+$idade = 18;
+$nome = "luiz";
+
+echo "Meu nome: $nome <br>";
+echo '<br>';
+echo 'minha idade é ' . $idade;
+
+var_dump($variavel03)
+/* SEGUNDO O GOOGLE, O var_dump:
+É uma função básica que retorna uma string e, dentro dela, 
+imprime os valores da raça e do nome deste objeto que está instanciado.*/
 ?>
